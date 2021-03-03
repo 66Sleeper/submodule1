2ce87c8612249c3566c20178f6802c2de023e152
66Sleeper <1149619184@qq.com>
2021-03-03 14:37:21 +0800

    refactor(SignIn.vue): add SignUp

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div><p>这里是子模块</p></div>
+    <div><p>这里是子模块</p></div>
+    <div><p>这里是子模块</p></div>
+    <div><p>这里是子模块</p></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
